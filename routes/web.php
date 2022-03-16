f478b8c10f3ce7708e91700fd96de9dcf9ff96b5
Kareem727 <kareem.ga.13@gmail.com>
2022-03-16 18:30:38 +0200

    V2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+route::get('/redirect',[HomeController::class,'redirect']);
